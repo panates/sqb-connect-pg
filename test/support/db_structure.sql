72526e320f3b56523671c0f23a8575fc77e19149
Eray Hanoğlu <erayszn@gmx.net>
2019-04-25 10:36:16 +0300

    Updated tests, Updated dependencies

--- test/support/db_structure.sql
+++ test/support/db_structure.sql
@@ -5,6 +5,7 @@ CREATE TABLE sqb_test.regions
 (
     id character varying(10) COLLATE pg_catalog."default" NOT NULL,
     name character varying(16) COLLATE pg_catalog."default",
+    tags character varying(16)[] COLLATE pg_catalog."default",
     CONSTRAINT regions_pkey PRIMARY KEY (id)
 );
 
